1963112af8500d8760f7956d182108e2911421d4
silqwer <silqwer@gmail.com>
2024-02-16 11:16:37 +0900

    feat: 정적 metadata

--- src/app/tag1/page.tsx
+++ src/app/tag1/page.tsx
@@ -1,5 +1,22 @@
 import React from "react";
 
+export async function generateMetadata() {
+  return {
+    openGraph: {
+      title: `오픈 그래프로 생성된 타이틀`,
+      description: "오픈 그래프로 생성된 description",
+      url: `https://twin.world`,
+      images: [
+        {
+          url: `https://picsum.photos/300`,
+          width: 300,
+          height: 300,
+        },
+      ],
+    },
+  };
+}
+
 export default function Tag1() {
   return <div>Tag1</div>;
 }
